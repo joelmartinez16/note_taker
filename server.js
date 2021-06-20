b2da068f3fffb66eff3a91c34b377fdbb1f6560d
Joel Martinez <joemartinez87@hotmail.com>
2021-06-19 18:25:50 -0700

    created delete button

--- server.js
+++ server.js
@@ -7,28 +7,19 @@ const path = require('path');
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-app.get('/notes', (req, res) => { 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
-
-app.get('/', (req, res) => { 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
-
-
+let getNotes= require('./db/db.json')
 app.get('/api/notes', (req, res) => {
-    const getNotes= require('./db/db.json')
+    // const getNotes= require('./db/db.json')
     res.json(getNotes)
 });
 app.post('/api/notes', (req, res) => {
-    const getNotes= require('./db/db.json')
+    // const getNotes= require('./db/db.json')
     const newNote= req.body
     newNote.id= uuidv4();
     getNotes.push(newNote)
@@ -41,11 +32,30 @@ app.post('/api/notes', (req, res) => {
 });
 
 
-app.delete('/api/notes', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
+    let id = req.params.id;
+    // const getNotes= require('./db/db.json')
+    const array = getNotes.filter(note => note.id !== id);
+    getNotes = array
+    fs.writeFile("./db/db.json", JSON.stringify(array)
+    ,function(err){
+        if (err) throw err
+        res.json(array)
+    })
  
- 
+});
+
+
+app.get('/notes', (req, res) => { 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+
